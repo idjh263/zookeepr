59d2387269cbfd4d96b1c2522847493b951b3a09
Irene Hipolito <irenehipolito@Irenes-MacBook-Air.local>
2022-01-24 22:41:25 -0500

    added get

--- server.js
+++ server.js
@@ -39,6 +39,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 };
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result; 
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -48,4 +53,14 @@ app.get('/api/animals', (req, res) => {
 })
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if(result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+    
 });
